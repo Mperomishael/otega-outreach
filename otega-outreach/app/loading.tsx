db67f7568e9ab8b24fe5b95791b7dd3ee449a0a6
Mperomishael <147824425+Mperomishael@users.noreply.github.com>
2025-04-19 11:17:11 +0100

    Delete otega-outreach directory

--- otega-outreach/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import CircularLoader from "@/components/ui/circular-loader"
-
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
-      <CircularLoader size="lg" label="Loading..." />
-    </div>
-  )
-}
