34b596b9871f8455e3588118c31872f5af6ea2db
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-06-24 03:27:19 -1100

    Update firebase-config.ts

--- lib/firebase-config.ts
+++ lib/firebase-config.ts
@@ -9,13 +9,12 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBiYjJZXi1TTNkjXqeZV4QbXQjlbHDG9dU",
-  authDomain: "otega-evangelical-outreach.firebaseapp.com",
-  projectId: "otega-evangelical-outreach",
-  storageBucket: "otega-evangelical-outreach.appspot.com",
-  messagingSenderId: "1098979571490",
-  appId: "1:1098979571490:web:9a9d3ec02be3f8b9a8d3a5",
-  measurementId: "G-VQPB2DLDL0",
+  apiKey: "AIzaSyB30iJ7xaBE7c0EoX4wBVhjW-xewwaXNg0",
+  authDomain: "otega-outreach.firebaseapp.com",
+  projectId: "otega-outreach",
+  storageBucket: "otega-outreach.firebasestorage.app",
+  messagingSenderId: "943328346091",
+  appId: "1:943328346091:web:1c5400d2998e48459063ae"
 }
 
 // Initialize Firebase
